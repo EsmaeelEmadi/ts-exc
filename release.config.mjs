3ab3d1a720664a65a23171015fe4be6c8488aeec
emad <emadi68moh@gmail.com>
2024-10-11 04:14:26 +0330

    feat(config): :sparkles: init
    project initiation

---
+++ release.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ["main", "next"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/git"
+    ]
+  };
